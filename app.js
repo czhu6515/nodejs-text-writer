1a6701cc3690aadf4d3b2150e89a5773112665d9
Chen <chenyu.zhu86@gmail.com>
2018-12-18 13:37:59 -0500

    awesome

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
+  const url = req.url
+  if (url === '/'){
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Press Me!</button></input></form></body>')
+    res.write('</html>')
+    return res.end()
+  }
   console.log(req.url, req.method, req.headers)
-  res.setHeader('Content-Type', 'text/html')
-  res.write('<html>')
-  res.write('<body><h1>Hello from node JS server!</h1></body>')
-  res.write('</html>')
-  res.end()
+  
 })
 
 server.listen(3200)
