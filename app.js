4064ec064d01cf2ba3907257fbce4810144a003e
Chen <chenyu.zhu86@gmail.com>
2018-12-19 22:59:14 -0500

    node js app completed

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
   const url = req.url
+  const method = req.method
   if (url === '/'){
     res.setHeader('Content-Type', 'text/html')
     res.write('<html>')
@@ -9,8 +11,21 @@ const server = http.createServer((req, res) => {
     res.write('</html>')
     return res.end()
   }
-  console.log(req.url, req.method, req.headers)
   
+  if (url === '/message' && method === 'POST'){
+    const body = []
+    req.on('data', (chunk) => {
+      body.push(chunk)
+    })
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString()
+      const message = parsedBody.split('=')[1]
+      fs.writeFileSync('message.txt', message)
+    })
+    res.statusCode = 302
+    res.setHeader('Location', '/')
+    return res.end()
+  }
 })
 
 server.listen(3200)
